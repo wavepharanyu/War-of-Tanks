e246d56a4c2bf02a409c526b4a5537d8a8b28b18
wavezzz <pharanyu.s@ku.th>
2017-09-08 16:09:41 +0700

    add ship

--- space game/space.py
+++ space game/space.py
@@ -13,7 +13,19 @@ class SpaceGameWindow(arcade.Window):
     def on_draw(self):
         arcade.start_render()
  
+    def __init__(self, width, height):
+        super().__init__(width, height)
  
+        arcade.set_background_color(arcade.color.BLACK)
+ 
+        self.ship = arcade.Sprite('images/ship.png')
+        self.ship.set_position(100, 100)
+        
+    def on_draw(self):
+        arcade.start_render()
+ 
+        self.ship.draw()    
+    
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
