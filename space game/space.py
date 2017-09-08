312b9604cd97c7690718990825fea0d7e2705f9c
wavezzz <pharanyu.s@ku.th>
2017-09-08 16:46:37 +0700

    move

--- space game/space.py
+++ space game/space.py
@@ -9,11 +9,21 @@ class SpaceGameWindow(arcade.Window):
  
         arcade.set_background_color(arcade.color.BLACK)
  
+        self.ship = arcade.Sprite('images/ship.png')
+        self.ship.set_position(100, 100)
  
     def on_draw(self):
         arcade.start_render()
  
+        self.ship.draw()
  
+    def update(self, delta):
+        ship = self.ship
+ 
+        if ship.center_y > SCREEN_HEIGHT:
+            ship.center_y = 0
+        ship.set_position(ship.center_x, ship.center_y + 5)
+
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
