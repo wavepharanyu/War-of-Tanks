bc84736870027038dedb839dd283c0a585fb75b4
wavezzz <pharanyu.s@ku.th>
2017-09-08 16:51:47 +0700

    debug

--- space game/models.py
+++ space game/models.py
@@ -1,21 +1,9 @@
 class Ship:
-    def __init__(self, world, x, y):
-        self.world = world
+    def __init__(self, x, y):
         self.x = x
         self.y = y
  
     def update(self, delta):
-        if self.y > self.world.height:
+        if self.y > 600:
             self.y = 0
         self.y += 5
-
-class World:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
- 
-        self.ship = Ship(self, 100, 100)
- 
- 
-    def update(self, delta):
-        self.ship.update(delta)
