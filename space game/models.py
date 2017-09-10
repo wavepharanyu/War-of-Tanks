754ce388de88120fa14704e91d20f3cb055efed4
wavezzz <pharanyu.s@ku.th>
2017-09-10 12:00:00 +0700

    gold

--- space game/models.py
+++ space game/models.py
@@ -1,11 +1,15 @@
 import arcade.key
-class Ship:
-    DIR_HORIZONTAL = 0
-    DIR_VERTICAL = 1
-    def __init__(self, world, x, y):
+class Model:
+    def __init__(self, world, x, y, angle):
         self.world = world
         self.x = x
         self.y = y
+        self.angle = 0
+class Ship(Model):
+    DIR_HORIZONTAL = 0
+    DIR_VERTICAL = 1
+    def __init__(self, world, x, y):
+        super().__init__(world, x, y, 0)
         self.direction = Ship.DIR_VERTICAL
         self.angle = 0
  
@@ -32,6 +36,7 @@ class World:
         self.width = width
         self.height = height
         self.ship = Ship(self, 100, 100)
+        self.gold = Gold(self, 400, 400)
  
  
     def update(self, delta):
@@ -40,3 +45,6 @@ class World:
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.SPACE:
             self.ship.switch_direction()
+class Gold(Model):
+    def __init__(self, world, x, y):
+        super().__init__(world, x, y, 0)
--- space game/space.py
+++ space game/space.py
@@ -13,6 +13,7 @@ class ModelSprite(arcade.Sprite):
     def sync_with_model(self):
         if self.model:
             self.set_position(self.model.x, self.model.y)
+            self.angle = self.model.angle
  
     def draw(self):
         self.sync_with_model()
@@ -27,14 +28,16 @@ class SpaceGameWindow(arcade.Window):
 
         self.world = World(width, height)
         self.ship_sprite = ModelSprite('images/ship.png',model=self.world.ship)
+        self.gold_sprite = ModelSprite('images/gold.png',model=self.world.gold)
  
     def on_draw(self):
         arcade.start_render()
+        self.gold_sprite.draw()
         self.ship_sprite.draw()
  
     def update(self, delta):
         self.world.update(delta)
-        
+
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
 
