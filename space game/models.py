830a9c578860f971160d07a78414b858d97fec88
wavezzz <pharanyu.s@ku.th>
2017-09-10 12:06:51 +0700

    hit gold

--- space game/models.py
+++ space game/models.py
@@ -1,10 +1,14 @@
 import arcade.key
+from random import randint
 class Model:
     def __init__(self, world, x, y, angle):
         self.world = world
         self.x = x
         self.y = y
         self.angle = 0
+    
+    def hit(self, other, hit_size):
+        return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
 class Ship(Model):
     DIR_HORIZONTAL = 0
     DIR_VERTICAL = 1
@@ -42,9 +46,16 @@ class World:
     def update(self, delta):
         self.ship.update(delta)
 
+        if self.ship.hit(self.gold, 15):
+            self.gold.random_location()
+
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.SPACE:
             self.ship.switch_direction()
 class Gold(Model):
     def __init__(self, world, x, y):
         super().__init__(world, x, y, 0)
+    
+    def random_location(self):
+        self.x = randint(0, self.world.width - 1)
+        self.y = randint(0, self.world.height - 1)
