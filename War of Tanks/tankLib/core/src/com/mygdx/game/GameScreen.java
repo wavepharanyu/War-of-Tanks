b478c037b674c0eb36fe1e6c0fa6a8ebdb01b3d1
wavezzz <pharanyu.s@ku.th>
2017-12-14 17:26:01 +0700

    press key and move

--- War of Tanks/tankLib/core/src/com/mygdx/game/GameScreen.java
+++ War of Tanks/tankLib/core/src/com/mygdx/game/GameScreen.java
@@ -1,37 +1,77 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
  
 public class GameScreen extends ScreenAdapter {
 	private TankGame tankGame;
-	public Texture tankImg;
+	public Texture tankImg1;
+	public Texture tankImg2;
 	public Texture background;
 	private World world;
 	private Player1 player1;
 	private Player2 player2;
+	private int x;
+	private int y;
 	
 	public GameScreen(TankGame tankGame) {
         this.tankGame = tankGame;
-        tankImg = new Texture("player.png");
+        tankImg1 = new Texture("player.png");
+        tankImg2 = new Texture("player2.png");
         world = new World(tankGame);
         player1 = world.getPlayer1();
         player2 = world.getPlayer2();
         background = new Texture("background.jpg");
+        x = 100;
+        y = 100;
     }
 	
 	 @Override
 	 public void render(float delta) {
+		 update(delta);
+		 Gdx.gl.glClearColor(0, 0, 0, 1);
+	     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		 SpriteBatch batch = tankGame.batch;
 	     batch.begin();
 	     batch.draw(background, 0, 0);
 	     Vector2 pos_player1 = player1.getPosition();
-	     batch.draw(tankImg, pos_player1.x, pos_player1.y);
+	     batch.draw(tankImg1, pos_player1.x, pos_player1.y);
 	     Vector2 pos_player2 = player2.getPosition();
-	     batch.draw(tankImg, pos_player2.x, pos_player2.y);
+	     batch.draw(tankImg2, pos_player2.x, pos_player2.y);
 	     batch.end();
 	 }
+	 
+	 public void update(float delta) {
+		 if(Gdx.input.isKeyPressed(Keys.A)) {
+			 player1.move(player1.DIRECTION_LEFT);
+	     }
+	     if(Gdx.input.isKeyPressed(Keys.D)) {
+	    	 player1.move(player1.DIRECTION_RIGHT);
+	     }
+	     if(Gdx.input.isKeyPressed(Keys.W)) {
+			 player1.move(player1.DIRECTION_UP);
+	     }
+	     if(Gdx.input.isKeyPressed(Keys.S)) {
+	    	 player1.move(player1.DIRECTION_DOWN);
+	     }
+	     if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+			 player2.move(player2.DIRECTION_LEFT);
+	     }
+	     if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+	    	 player2.move(player2.DIRECTION_RIGHT);
+	     }
+	     if(Gdx.input.isKeyPressed(Keys.UP)) {
+			 player2.move(player2.DIRECTION_UP);
+	     }
+	     if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+	    	 player2.move(player2.DIRECTION_DOWN);
+	     }
+	 }
+	
  
 }
--- War of Tanks/tankLib/core/src/com/mygdx/game/Player2.java
+++ War of Tanks/tankLib/core/src/com/mygdx/game/Player2.java
@@ -4,6 +4,19 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Player2 {
 	private Vector2 position;
+	private static final int [][] DIR_OFFSETS = new int [][] {
+        {0,0},
+        {0,-1},
+        {1,0},
+        {0,1},
+        {-1,0}
+    };
+    public static final int SPEED = 10;
+    public static final int DIRECTION_LEFT = 4;
+    public static final int DIRECTION_UP = 3;
+    public static final int DIRECTION_RIGHT = 2;
+    public static final int DIRECTION_DOWN = 1;
+    public static final int DIRECTION_STILL = 0;
 	
 	public Player2(int x , int y) {
 		position = new Vector2(x,y);
@@ -12,4 +25,9 @@ public class Player2 {
 	public Vector2 getPosition() {
 		return position;
 	}
+	
+	public void move(int dir) { 
+        position.x += SPEED * DIR_OFFSETS[dir][0];
+        position.y += SPEED * DIR_OFFSETS[dir][1];
+    }
 }
